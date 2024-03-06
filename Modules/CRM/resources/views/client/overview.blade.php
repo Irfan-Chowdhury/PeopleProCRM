d4f674850cb9faa81ac15839dc8cd37b2d12a9a2
Irfan Chowdhury <irfanchowdhury80@gmail.com>
2024-03-06 15:31:30 +0600

    client Overview

--- Modules/CRM/resources/views/client/overview.blade.php
+++ Modules/CRM/resources/views/client/overview.blade.php
@@ -4,24 +4,92 @@
 <section>
     <div class="container-fluid">
         <div class="d-flex justify-content-between mb-30px">
-            <div><h1 class="thin-text">{{trans('file.Welcome')}} </h1></div>
+            <div><h1 class="thin-text">{{trans('file.Overview')}} </h1></div>
         </div>
     </div>
-    <div class="row">
-        <div class="col-sm-2">
-            <div class="wrapper count-title text-center">
-                <a href="">
-                    <div class="name"><strong class="purple-text">{{ trans('file.Total Client') }}</strong></div>
-                    <div class="count-number employee-count">{{count($clients)}}</div>
-                </a>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4">
+                <div class="wrapper count-title text-center">
+                    <a href="">
+                        <div class="name"><strong class="purple-text">{{ trans('file.Total Client') }}</strong></div>
+                        <div class="count-number employee-count">{{count($clients)}}</div>
+                    </a>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="wrapper count-title text-center">
+                    <a href="">
+                        <div class="name"><strong class="purple-text">{{ trans('file.Total Subscription') }}</strong></div>
+                        <div class="count-number employee-count">{{count($subscriptions)}}</div>
+                    </a>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="wrapper count-title text-center">
+                    <a href="">
+                        <div class="name"><strong class="purple-text">{{ trans('file.Total Contract') }}</strong></div>
+                        <div class="count-number employee-count">{{count($contracts)}}</div>
+                    </a>
+                </div>
             </div>
         </div>
-        <div class="col-sm-2">
-            <div class="wrapper count-title text-center">
-                <a href="">
-                    <div class="name"><strong class="purple-text">{{ trans('file.Total Subscription') }}</strong></div>
-                    <div class="count-number employee-count">{{count($subscriptions)}}</div>
-                </a>
+        <div class="row mt-3">
+            <div class="col-md-4">
+                <div class="wrapper count-title text-left">
+                    <a href="">
+                        <div class="name"><strong class="purple-text">{{ trans('file.Clients have paid invoices') }}</strong></div>
+                        <div class="progress" style="height: 20px;">
+                            <div class="progress-bar progress-bar-striped" style="width:{{ $invoices->paid_percentage }}%">{{ $invoices->paid_percentage }}%</div>
+                        </div>
+                    </a>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="wrapper count-title text-left">
+                    <a href="">
+                        <div class="name"><strong class="purple-text">{{ trans('file.Clients have unpaid invoices') }}</strong></div>
+                        <div class="progress" style="height: 20px;">
+                            <div class="progress-bar progress-bar-striped" style="width:{{ $invoices->unpaid_percentage }}%">{{ $invoices->unpaid_percentage }}%</div>
+                        </div>
+                    </a>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="wrapper count-title text-left">
+                    <a href="">
+                        <div class="name"><strong class="purple-text">{{ trans('file.Clients have sent invoices') }}</strong></div>
+                        <div class="progress" style="height: 20px;">
+                            <div class="progress-bar progress-bar-striped" style="width:{{ $invoices->sent_percentage }}%">{{ $invoices->sent_percentage }}%</div>
+                        </div>
+                    </a>
+                </div>
+            </div>
+        </div>
+        <div class="row mt-3">
+            <div class="col-md-4">
+                <div class="wrapper count-title text-center">
+                    <a href="">
+                        <div class="name"><strong class="purple-text">{{ trans('file.Total Pending Order') }}</strong></div>
+                        <div class="count-number employee-count">{{$orderResult->totalPendingOrder}}</div>
+                    </a>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="wrapper count-title text-center">
+                    <a href="">
+                        <div class="name"><strong class="purple-text">{{ trans('file.Total Completed Order') }}</strong></div>
+                        <div class="count-number employee-count">{{$orderResult->totalCompletedOrder}}</div>
+                    </a>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="wrapper count-title text-center">
+                    <a href="">
+                        <div class="name"><strong class="purple-text">{{ trans('file.Total Canceled Order') }}</strong></div>
+                        <div class="count-number employee-count">{{$orderResult->totalCanceledOrder}}</div>
+                    </a>
+                </div>
             </div>
         </div>
     </div>
--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -3,12 +3,14 @@
 namespace App\Http\Controllers;
 
 use App\Models\Client;
+use App\Models\CRM\Invoice;
 use App\Models\User;
 use Exception;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Validator;
+use Modules\CRM\App\Models\Contract;
 use Modules\CRM\App\Models\Subscription;
 
 class ClientController extends Controller {
@@ -17,8 +19,34 @@ class ClientController extends Controller {
     {
         $clients = Client::where('is_active',true)->get();
         $subscriptions = Subscription::all();
+        $contracts = Contract::all();
 
-        return view('crm::client.overview', compact('clients','subscriptions'));
+        $invoices = DB::table(DB::raw('(SELECT
+            COUNT(*) AS total_clients,
+            SUM(CASE WHEN status = 1 THEN 1 ELSE 0 END) AS paid_clients,
+            SUM(IF(status = 2, 1, 0)) AS unpaid_clients,
+            SUM(IF(status != 1 AND status != 2 , 1, 0)) AS sent_clients
+        FROM
+            invoices) AS subquery'))
+        ->select([
+            DB::raw('ROUND((paid_clients / total_clients) * 100) AS paid_percentage'),
+            DB::raw('ROUND((unpaid_clients / total_clients) * 100 ) AS unpaid_percentage'),
+            DB::raw('ROUND((sent_clients / total_clients) * 100 )AS sent_percentage')
+        ])->first();
+
+
+        $orderResult = DB::table('orders')
+            ->selectRaw('
+                SUM(IF(status = "pending", 1, 0)) AS totalPendingOrder,
+                SUM(IF(status = "completed", 1, 0)) AS totalCompletedOrder,
+                SUM(IF(status = "canceled", 1, 0)) AS totalCanceledOrder
+            ')
+            ->first();
+
+
+
+
+        return view('crm::client.overview', compact('clients','subscriptions','contracts','invoices','orderResult'));
     }
 
 
